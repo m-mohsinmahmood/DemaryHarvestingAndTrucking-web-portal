47e6f80a8040498fb914aacab9d67aa190339240
Momin Bin Shahid <mshahid@Momins-MacBook-Pro.local>
2022-08-01 16:15:25 +0500

    Remove text from Dashboard and update dropdown

--- src/app/modules/admin/dashboards/project/project.component.ts
+++ src/app/modules/admin/dashboards/project/project.component.ts
@@ -19,7 +19,7 @@ export class ProjectComponent implements OnInit, OnDestroy
     chartMonthlyExpenses: ApexOptions = {};
     chartYearlyExpenses: ApexOptions = {};
     data: any;
-    selectedProject: string = 'ACME Corp. Backend App';
+    selectedProject: string = 'TAG Solutions';
     private _unsubscribeAll: Subject<any> = new Subject<any>();
 
     /**
