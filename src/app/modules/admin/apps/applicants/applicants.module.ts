0b7b3cd5390f59eab95ac4bc179e2b813e520a30
Ammad Aslam <aaslam@cinnova.local>
2022-08-04 22:43:05 +0500

    applicant Module added , created Stepper form

---
+++ src/app/modules/admin/apps/applicants/applicants.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { SharedModule } from 'app/shared/shared.module';
+import { applicantsRoutes } from 'app/modules/admin/apps/applicants/applicants.routing';
+import { ApplicantsComponent } from 'app/modules/admin/apps/applicants/applicants.component';
+import { ApplicantsListComponent } from 'app/modules/admin/apps/applicants/list/list.component';
+import { AddComponent } from './add/add.component';
+
+
+@NgModule({
+    declarations: [
+        ApplicantsComponent,
+        ApplicantsListComponent,
+        AddComponent
+    ],
+    imports     : [
+        RouterModule.forChild(applicantsRoutes),
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatStepperModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatRippleModule,
+        MatSortModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatTooltipModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatMomentDateModule,
+        SharedModule
+    ]
+})
+export class ApplicantsModule
+{
+}
