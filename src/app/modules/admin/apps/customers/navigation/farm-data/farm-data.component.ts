41a2330f156b78e7de69f6631a0ef02e60b4fce0
Ammad Aslam <aaslam@cinnova.local>
2022-09-15 14:58:34 +0500

    farm data ui updated

--- src/app/modules/admin/apps/customers/navigation/farm-data/farm-data.component.ts
+++ src/app/modules/admin/apps/customers/navigation/farm-data/farm-data.component.ts
@@ -7,17 +7,17 @@ import { Input, Component, OnInit } from '@angular/core';
     /* language=SCSS */
     `
         .farm-data-grid {
-            grid-template-columns: 10% 50% 30%;
+            grid-template-columns: 25% 25 25% 25%;
 
             @screen sm {
                 grid-template-columns: 10% 20% 20% 20% 15% 10% 10%;
             }
             @screen md {
-                grid-template-columns: 10% 20% 20% 20% 15% 10% 10% ;
+                grid-template-columns: 25% 25 25% 25%;
             }
 
             @screen lg {
-                grid-template-columns: 10% 20% 20% 20% 15% 10% 10%;
+                grid-template-columns: 20% 30% 30% 20% ;
             }
         }
     `
