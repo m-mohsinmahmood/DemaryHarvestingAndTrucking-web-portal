ef4a3b13c48f8aa6a22f2fff5c03029924011b13
Asad Amanullah <aamanullah@cinnova.local>
2022-10-05 13:35:03 +0500

    [Customers Module]: Changes and Fixes

--- src/app/modules/admin/apps/customers/navigation/invoice/invoice.component.ts
+++ src/app/modules/admin/apps/customers/navigation/invoice/invoice.component.ts
@@ -4,6 +4,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { FuseConfirmationService } from '@fuse/services/confirmation';
 import { AddItemComponent } from './add-item/add-item.component';
+import html2canvas from 'html2canvas';
+import jspdf from 'jspdf';
+
 
 @Component({
   selector: 'app-invoice',
@@ -14,6 +17,8 @@ export class InvoiceComponent implements OnInit {
 
   form: FormGroup;
   isEditMode: boolean = false;
+  pdfTable: any;
+
 
 
 
@@ -66,6 +71,18 @@ export class InvoiceComponent implements OnInit {
                  .subscribe((result) => {
                      console.log('Compose dialog was closed!');
                  });
+    }
+
+    exportAsPDF(MyDIv)
+    {
+        let data = document.getElementById('MyDIv');  
+        html2canvas(data).then(canvas => {
+        const contentDataURL = canvas.toDataURL('image/png')  // 'image/jpeg' for lower quality output.
+        let pdf = new jspdf('l', 'cm', 'a4'); //Generates PDF in landscape mode
+        // let pdf = new jspdf('p', 'cm', 'a4'); Generates PDF in portrait mode
+        pdf.addImage(contentDataURL, 'PNG', 0, 0, 29.7, 21.0);  
+        pdf.save('Filename.pdf');   
+      }); 
     }
 
 }
