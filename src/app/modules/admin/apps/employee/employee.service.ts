08f927bb77f0b9fa944354a3ec6bce7523beb279
Ammad Aslam <aaslam@cinnova.local>
2022-12-09 20:28:21 +0500

    api link changed , resolvers removed , employee service functions updated

--- src/app/modules/admin/apps/employee/employee.service.ts
+++ src/app/modules/admin/apps/employee/employee.service.ts
@@ -12,9 +12,7 @@ import { AlertService } from 'app/core/alert/alert.service';
 export class EmployeeService {
     // Private
     public navigationLabels = employeeNavigation;
-    public _employeedata: BehaviorSubject<Employee | null> = new BehaviorSubject(null);
-    private _pagination: BehaviorSubject<EmployeePagination | null> = new BehaviorSubject(null);
-    private _employeesdata: BehaviorSubject<Employee[] | null> = new BehaviorSubject(null);
+    public  _employeedata: BehaviorSubject<Employee | null> = new BehaviorSubject(null);
     private _countries: BehaviorSubject<Country[] | null> = new BehaviorSubject(null);
     private _documents: BehaviorSubject<Documents | null> = new BehaviorSubject(null);
     private _item: BehaviorSubject<Item | null> = new BehaviorSubject(null);
@@ -25,68 +23,43 @@ export class EmployeeService {
     //#endregion
 
     // #region Applicants & Applicant
-
     // Data
-    private employeesList: BehaviorSubject<any[] | null> = new BehaviorSubject(null);
-    readonly employeesList$: Observable<any[] | null> = this.employeesList.asObservable();
+    private employeeList: BehaviorSubject<any[] | null> = new BehaviorSubject(null);
+    readonly employeeList$: Observable<any[] | null> = this.employeeList.asObservable();
 
-    private employeeList: BehaviorSubject<any | null> = new BehaviorSubject(null);
-    readonly employeeList$: Observable<any | null> = this.employeeList.asObservable();
+    private employee: BehaviorSubject<any | null> = new BehaviorSubject(null);
+    readonly employee$: Observable<any | null> = this.employee.asObservable();
 
     // Loaders
-    private isLoadingEmployeesList: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    readonly isLoadingEmployeesList$: Observable<boolean> = this.isLoadingEmployeesList.asObservable();
-
-    isLoadingEmployeeList: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    private isLoadingEmployeeList: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     readonly isLoadingEmployeeList$: Observable<boolean> = this.isLoadingEmployeeList.asObservable();
+
+    isLoadingEmployee: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    readonly isLoadingEmployee$: Observable<boolean> = this.isLoadingEmployee.asObservable();
     // #endregion
 
-    //#region 
     constructor(
         private _httpClient: HttpClient,
         private _alertSerice: AlertService
-    ) {
-    }
-
-    /**
-     * Getter for pagination
-     */
-    get pagination$(): Observable<EmployeePagination> {
-        return this._pagination.asObservable();
-    }
-
-    /**
-     * Getter for employee
-     */
-    get employee$(): Observable<Employee> {
-        return this._employeedata.asObservable();
-    }
-
-    /**
-     * Getter for employees
-     */
-    get employeedata$(): Observable<Employee[]> {
-        return this._employeesdata.asObservable();
-    }
+    ) {}
+    
+    //#region Getter
 
     get countries$(): Observable<Country[]> {
         return this._countries.asObservable();
     }
 
-    /**
-     * Getter for items
-     */
     get documents$(): Observable<Documents> {
         return this._documents.asObservable();
     }
 
-    /**
-     * Getter for item
-     */
     get item$(): Observable<Item> {
         return this._item.asObservable();
     }
 
+    //#endregion
+
+    //#region Error 
     handleError(error: HttpErrorResponse) {
         let errorMessage = 'Unknown error!';
         if (error.error instanceof ErrorEvent) {
@@ -114,46 +87,57 @@ export class EmployeeService {
         }
         return throwError(errorMessage);
     }
+    //#endregion
 
 
     //#region Applicant API's 
-    // getEmployees(page: number = 1, limit: number = 10, sort: string = '', order: 'asc' | 'desc' | '' = '', search: string = '') {
-    //     let params = new HttpParams();
-    //     params = params.set('page', page);
-    //     params = params.set('limit', limit);
-    //     params = params.set('search', search);
-    //     params = params.set('sort', sort);
-    //     params = params.set('order', order);
-    //     return this._httpClient
-    //         .get<any>(`api-1/employees`, {
-    //             params,
-    //         })
-    //         .pipe(take(1))
-    //         .subscribe(
-    //             (res: any) => {
-    //                 this.isLoadingEmployeesList.next(true);
-    //                 this.employeesList.next(res);
-    //                 this.isLoadingEmployeesList.next(false);
-    //             },
-    //             (err) => {
-    //                 this.handleError(err);
-    //             }
-    //         );
-    // }
-    // getEmployeeById(id: string) {
-    //     return this._httpClient
-    //         .get(`api-1/employee?id=${id}`)
-    //         .pipe(take(1));
-    // }
+    getEmployees(page: number = 1, limit: number = 10, sort: string = '', order: 'asc' | 'desc' | '' = '', search: string = '') {
+        let params = new HttpParams();
+        params = params.set('page', page);
+        params = params.set('limit', limit);
+        params = params.set('search', search);
+        params = params.set('sort', sort);
+        params = params.set('order', order);
+        return this._httpClient
+            .get<any>(`api-1/employee`, {
+                params,
+            })
+            .pipe(take(1))
+            .subscribe(
+                (res: any) => {
+                    this.isLoadingEmployeeList.next(true);
+                    this.employeeList.next(res);
+                    this.isLoadingEmployeeList.next(false);
+                },
+                (err) => {
+                    this.handleError(err);
+                }
+            );
+    }
+    getEmployeeById(id: string) {
+        return this._httpClient
+            .get(`api-1/employee?id=${id}`)
+            .pipe(take(1))
+            .subscribe(
+                (res: any) => {
+                    this.isLoadingEmployee.next(true);
+                    this.employee.next(res);
+                    this.isLoadingEmployee.next(false);
+                },
+                (err) => {
+                    this.handleError(err);
+                }
+            );
+    }
 
     createEmployee(data: any) {
         this._httpClient
-            .post(`api-1/employees`, data)
+            .post(`api-1/employee`, data)
             .pipe(take(1))
             .subscribe(
                 (res: any) => {
                     this.closeDialog.next(true);
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                     //show notification based on message returned from the api
                     this._alertSerice.showAlert({
                         type: 'success',
@@ -167,7 +151,7 @@ export class EmployeeService {
                 (err) => {
                     this.handleError(err);
                     this.closeDialog.next(false);
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                 },
                 () => {
                     this.getEmployees();
@@ -176,11 +160,11 @@ export class EmployeeService {
     }
     updateEmployee(data: any) {
         this._httpClient
-            .put(`api-1/employees`, data)
+            .put(`api-1/employee`, data)
             .pipe(take(1))
             .subscribe(
                 (res: any) => {
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                     this.closeDialog.next(true);
                     this._alertSerice.showAlert({
                         type: 'success',
@@ -194,7 +178,7 @@ export class EmployeeService {
                 (err) => {
                     this.handleError(err);
                     this.closeDialog.next(false);
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                 },
                 () => {
                     this.getEmployees();
@@ -218,7 +202,7 @@ export class EmployeeService {
             .pipe(take(1))
             .subscribe(
                 (res: any) => {
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                     this.closeDialog.next(true);
                     this._alertSerice.showAlert({
                         type: 'success',
@@ -232,7 +216,7 @@ export class EmployeeService {
                 (err) => {
                     this.handleError(err);
                     this.closeDialog.next(false);
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                 },
                 () => {
                     this.getEmployeeById(newData.employee_data.id);
@@ -246,14 +230,14 @@ export class EmployeeService {
             .pipe(take(1))
             .subscribe(
                 (res: any) => {
-                    this.isLoadingEmployeeList.next(true);
+                    this.isLoadingEmployee.next(true);
                 },
                 (err) => {
                     this.handleError(err);
                 },
                 () => {
                     this.getEmployees();
-                    this.isLoadingEmployeeList.next(false);
+                    this.isLoadingEmployee.next(false);
                 }
             );
     }
@@ -270,6 +254,7 @@ export class EmployeeService {
 
     //#endregion
 
+    //#region get Items
     getItems(folderId: string | null = null): Observable<Item[]> {
         return this._httpClient.get<Documents>('api/apps/employee/details', { params: { folderId } }).pipe(
             tap((response: any) => {
@@ -303,154 +288,6 @@ export class EmployeeService {
         );
     }
     //#endregion
-
-
-    /**
-     * Get employees
-     *
-     *
-     * @param page
-     * @param size
-     * @param sort
-     * @param order
-     * @param search
-     */
-    getEmployees(page: number = 0, size: number = 10, sort: string = 'name', order: 'asc' | 'desc' | '' = 'asc', search: string = ''):
-        Observable<{ pagination: EmployeePagination; products: Employee[] }> {
-        return this._httpClient.get<{ pagination: EmployeePagination; products: Employee[] }>('api/apps/employee', {
-            params: {
-                page: '' + page,
-                size: '' + size,
-                sort,
-                order,
-                search
-            }
-        }).pipe(
-            tap((response) => {
-                this._pagination.next(response.pagination);
-                this._employeesdata.next(response.products);
-            })
-        );
-    }
-
-    /**
-     * Get employee by id
-     */
-    getEmployeeById(id: string): Observable<Employee> {
-        return this._employeesdata.pipe(
-            take(1),
-            map((employees) => {
-                // Find the employee
-                const employee = employees.find(item => item.id === id) || null;
-
-                // Update the employee
-                this._employeedata.next(employee);
-
-                // Return the employee
-                return employee;
-            }),
-            switchMap((employee) => {
-
-                if (!employee) {
-                    return throwError('Could not found product with id of ' + id + '!');
-                }
-
-                return of(employee);
-            })
-        );
-    }
-
-
-
-    /**
-     * Create product
-     */
-    // createEmployee(): Observable<Employee> {
-    //     return this.employeedata$.pipe(
-    //         take(1),
-    //         switchMap(employees => this._httpClient.post<Employee>('api/apps/employee/product', {}).pipe(
-    //             map((newEmployee) => {
-
-    //                 // Update the employees with the new product
-    //                 this._employeesdata.next([newEmployee, ...employees]);
-
-    //                 // Return the new employee
-    //                 return newEmployee;
-    //             })
-    //         ))
-    //     );
-    // }
-
-    /**
-     * Update employee
-     *
-     * @param id
-     * @param product
-     */
-    // updateEmployee(id: string, product: Employee): Observable<Employee> {
-    //     return this.employeedata$.pipe(
-    //         take(1),
-    //         switchMap(employees => this._httpClient.patch<Employee>('api/apps/employee/product', {
-    //             id,
-    //             product
-    //         }).pipe(
-    //             map((updatedEmployee) => {
-
-    //                 // Find the index of the updated employee
-    //                 const index = employees.findIndex(item => item.id === id);
-
-    //                 // Update the employee
-    //                 employees[index] = updatedEmployee;
-
-    //                 // Update the employees
-    //                 this._employeesdata.next(employees);
-
-    //                 // Return the updated employee
-    //                 return updatedEmployee;
-    //             }),
-    //             switchMap(updatedEmployee => this.employee$.pipe(
-    //                 take(1),
-    //                 filter(item => item && item.id === id),
-    //                 tap(() => {
-
-    //                     // Update the employee if it's selected
-    //                     this._employeedata.next(updatedEmployee);
-
-    //                     // Return the updated employee
-    //                     return updatedEmployee;
-    //                 })
-    //             ))
-    //         ))
-    //     );
-    // }
-
-    /**
-     * Delete the employee
-     *
-     * @param id
-     */
-    // deleteEmployee(id: string): Observable<boolean> {
-    //     return this.employeedata$.pipe(
-    //         take(1),
-    //         switchMap(employees => this._httpClient.delete('api/apps/employee/product', { params: { id } }).pipe(
-    //             map((isDeleted: boolean) => {
-
-    //                 // Find the index of the deleted employee
-    //                 const index = employees.findIndex(item => item.id === id);
-
-    //                 // Delete the employee
-    //                 employees.splice(index, 1);
-
-    //                 // Update the employees
-    //                 this._employeesdata.next(employees);
-
-    //                 // Return the deleted status
-    //                 return isDeleted;
-    //             })
-    //         ))
-    //     );
-    // }
-
 
 
 }
