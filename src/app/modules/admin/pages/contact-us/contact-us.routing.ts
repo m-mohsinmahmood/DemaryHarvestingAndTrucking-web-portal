615cb8f7f0d4afd55b28ca65f35b1ddebf721dc9
Asad Amanullah <aamanullah@cinnova.local>
2022-12-27 21:09:46 +0500

    Static pages work

---
+++ src/app/modules/admin/pages/contact-us/contact-us.routing.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { ContactUsComponent } from './contact-us.component';
+
+export const activitiesRoutes: Route[] = [
+    {
+        path     : '',
+        component: ContactUsComponent,
+
+    }
+];
---
+++ src/app/modules/admin/pages/services-page/services-page.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { ServicesPageComponent } from './services-page.component';
+export const activitiesRoutes: Route[] = [
+    {
+        path     : '',
+        component: ServicesPageComponent,
+
+    }
+];
