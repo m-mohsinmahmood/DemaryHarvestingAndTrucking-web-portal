40d316d42bdc62e10d179aa268938cbc2033eabb
Asad Amanullah <aamanullah@cinnova.local>
2023-01-10 00:45:00 +0500

    Pages changes and article page

---
+++ src/app/modules/admin/pages/performance-article/performance-article.routing.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { PerformanceArticleComponent } from './performance-article.component';
+
+export const activitiesRoutes: Route[] = [
+    {
+        path     : '',
+        component: PerformanceArticleComponent,
+
+    }
+];
