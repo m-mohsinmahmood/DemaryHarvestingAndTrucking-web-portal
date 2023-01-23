e046c9e1feae7b2a6659651103d00659b89896ba
Asad Amanullah <aamanullah@cinnova.local>
2023-01-23 13:27:16 +0500

    Payroll work

--- src/app/modules/admin/apps/employee/navigation/payroll/payroll.component.ts
+++ src/app/modules/admin/apps/employee/navigation/payroll/payroll.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { EmployeeService } from '../../employee.service';
+import { Employee } from '../../employee.types';
 
 @Component({
   selector: 'app-payroll',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payroll.component.scss']
 })
 export class PayrollComponent implements OnInit {
+  //#region Observable
+  employeeDwr$: Observable<Employee>;
+  routeID; // URL ID
+  @Input () employee: any;
 
-  constructor() { }
+
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
+  //#endregion
+
+  constructor(
+    private _employeeService: EmployeeService,
+    public activatedRoute: ActivatedRoute,
+
+
+
+  ) { }
 
   ngOnInit(): void {
+    this.getRouteParams();
+    this.initApis();
+    this.initObservables();
+    console.log(this.employeeDwr$);
   }
+
+  
+  //#region route params function
+  getRouteParams(){
+    this.activatedRoute.params.subscribe((params) => {
+      this.routeID = params.Id;
+    });
+  }
+  //#endregion
+
+  //#region Init Observables
+  initObservables() {
+    this.employeeDwr$ = this._employeeService.employeeDwr$;
+
+  }
+
+  //#endregion
+
+  //#region Init Apis
+  initApis() {
+    this._employeeService.getPayrollById(this.routeID);
+  }
+  //#endregion
 
 }
