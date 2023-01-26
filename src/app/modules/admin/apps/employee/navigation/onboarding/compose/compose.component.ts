832ed43a651ee87736d432bffa23c2987daca832
Ammad Aslam <aaslam@cinnova.local>
2023-01-26 21:20:23 +0500

    cdl training added and radio button added in drivers license

--- src/app/modules/admin/apps/employee/navigation/onboarding/compose/compose.component.ts
+++ src/app/modules/admin/apps/employee/navigation/onboarding/compose/compose.component.ts
@@ -66,31 +66,31 @@ export class MailboxComposeComponent implements OnInit {
         this.emails = [
             {
                 id: '1', subject: 'DHT “Onboarding Process” (Step 1 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ',</br>We appreciate your interest in DHT and are looking forward to working with you this upcoming season. To complete your pre-employment (onboarding) process in compliance with US Government regulations, we are required to collect several forms. This email is the first of several steps, you will need to complete to become employed by DHT.  Please note that all your personal information and documentation is confidential, protected and can be accessed anytime from your personal online DHT account.</br></br>Please visit your newly set-up DHT account at demarayharvesting.com and begin the process by uploading your social security number (Payroll Document Folder) and driver’s license (DOT Document Folder) information.  Thanks again for your patience and timely cooperation in gathering all required documentation.</br></br>Kind regards, '
+                email: 'Dear ' + this.data.employee.first_name + ',</br></br>We appreciate your interest in DHT and are looking forward to working with you this upcoming season. To complete your pre-employment (onboarding) process in compliance with US Government regulations, we are required to collect several forms. This email is the first of several steps, you will need to complete to become employed by DHT.  Please note that all your personal information and documentation is confidential, protected and can be accessed anytime from your personal online DHT account.</br></br>Please visit your newly set-up DHT account at demarayharvesting.com and begin the process by uploading your social security number (Payroll Document Folder) and driver’s license (DOT Document Folder) information.  Thanks again for your patience and timely cooperation in gathering all required documentation.</br></br>Kind regards, '
             },
             {
                 id: '2', subject: 'DHT “Onboarding Process” (Step 2 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ',</br>Thanks for successfully uploading your passport and driver’s license.  Next, we will need you to carefully review, read, and electronically sign the four compliance related documents below.  Your electronic signature will be used as verification that you’ve read and understood the contents of these documents. Thanks for your help in gathering this documentation.</br></br> Ag Work Agreement (DHS Documents Folder) </br> Work itinerary (DHS Documents Folder) </br> DHT work rules (Company Documents Folder) </br> Handbook (Company Documents Folder)</br></br> Kind regards,'
+                email: 'Dear ' + this.data.employee.first_name + ',</br></br>Thanks for successfully uploading your passport and driver’s license.  Next, we will need you to carefully review, read, and electronically sign the four compliance related documents below.  Your electronic signature will be used as verification that you’ve read and understood the contents of these documents. Thanks for your help in gathering this documentation.</br></br> Ag Work Agreement (DHS Documents Folder) </br> Work itinerary (DHS Documents Folder) </br> DHT work rules (Company Documents Folder) </br> Handbook (Company Documents Folder)</br></br> Kind regards,'
             },
             {
                 id: '3', subject: 'DHT “Onboarding Process” (Step 3 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ', </br>Thanks for successfully signing the required DHT and US Government compliance documentation. Next, we will we need you to review, complete, and electronically sign the attached DHT Contract Letter (DOI Document Folder) and W4 tax (Payroll Document Folder) form.</br></br>Kind regards,'
+                email: 'Dear ' + this.data.employee.first_name + ', </br></br>Thanks for successfully signing the required DHT and US Government compliance documentation. Next, we will we need you to review, complete, and electronically sign the attached DHT Contract Letter (DOI Document Folder) and W4 tax (Payroll Document Folder) form.</br></br>Kind regards,'
             },
             {
                 id: '4', subject: 'DHT “Onboarding Process” (Step 4 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ', </br>Thanks for successfully signing the DHT Contract Letter and W4 tax form.  Next, to ensure that there are no delays in getting paid, we require all employees set-up an online bank account (Payroll Document Folder). Once you’ve successfully created an online bank account, you will need to visit your DHT online account at demarayharvesting.com to enter the appropriate bank account information.</br></br>Kind regards,'
+                email: 'Dear ' + this.data.employee.first_name + ', </br></br>Thanks for successfully signing the DHT Contract Letter and W4 tax form.  Next, to ensure that there are no delays in getting paid, we require all employees set-up an online bank account (Payroll Document Folder). Once you’ve successfully created an online bank account, you will need to visit your DHT online account at demarayharvesting.com to enter the appropriate bank account information.</br></br>Kind regards,'
             },
             {
                 id: '5', subject: 'DHT “Onboarding Process” (Step 5 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ', </br>Thanks for setting up your online bank account.  You are almost finished with the pre-employment (onboarding) process.</br></br>Next, we will need you to carefully review, read, and electronically sign and date the two required DHT compliance documents below.  Thanks again for your help in completing this step.</br></br>Drug Policy (Company Documents Folder) </br>Reprimand Policy (Company Documents Folder)</br></br>Kind regards,'
+                email: 'Dear ' + this.data.employee.first_name + ', </br></br>Thanks for setting up your online bank account.  You are almost finished with the pre-employment (onboarding) process.</br></br>Next, we will need you to carefully review, read, and electronically sign and date the two required DHT compliance documents below.  Thanks again for your help in completing this step.</br></br>Drug Policy (Company Documents Folder) </br>Reprimand Policy (Company Documents Folder)</br></br>Kind regards,'
             },
             {
                 id: '6', subject: 'DHT “Onboarding Process” (Step 6 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ', </br>Thanks for completing the recent compliance documents.</br></br>In advance of your scheduled start date, we ask all employees who do not currently have their CDL license to review the attached CDL training instructions and website to help study and prepare for their CDL test upon arrival.</br></br>Kind regards,'
+                email: 'Dear ' + this.data.employee.first_name + ', </br></br>Thanks for completing the recent compliance documents.</br></br>In advance of your scheduled start date, we ask all employees who do not currently have their CDL license to review the attached CDL training instructions and website to help study and prepare for their CDL test upon arrival.</br></br>CDL Training Instructions</br>CDL Training Videos Website </br></br>Kind regards,'
             },
             {
                 id: '7', subject: 'DHT “Onboarding Process” (Step 7 of 7)',
-                email: 'Dear ' + this.data.employee.first_name + ', </br>Congratulations you are now officially ready to start your DHT job in the upcoming Harvesting season.  Please reach out to your recruiter to schedule your arrival date and make related travel arrangements.  We are excited to welcome you to the US and into the DHT family.  Please don’t hesitate to reach out if you have any further questions before your scheduled start date.See you soon!</br></br>Kind regards,'
+                email: 'Dear ' + this.data.employee.first_name + ', </br></br>Congratulations you are now officially ready to start your DHT job in the upcoming Harvesting season.  Please reach out to your recruiter to schedule your arrival date and make related travel arrangements.  We are excited to welcome you to the US and into the DHT family.  Please don’t hesitate to reach out if you have any further questions before your scheduled start date.See you soon!</br></br>Kind regards,'
             },
         ];
     }
@@ -126,7 +126,7 @@ export class MailboxComposeComponent implements OnInit {
             this.patchEmail(11, 4)
         }
         else if (this.current_status_step == '12') {
-            this.patchEmail(13, 6)
+            this.patchEmail(13, 5)
         }
         else if (this.current_status_step == '14') {
             this.patchEmail(15, 6)
